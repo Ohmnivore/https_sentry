93698610c13ac88a69fcf2ffa0b1075baf9a47f0
Mark Beiline <markbeiline@gmail.com>
2018-02-04 01:12:51 -0500

    Style improvements

--- crawler.py
+++ crawler.py
@@ -47,11 +47,11 @@ class CrawlerResult:
 
 class Crawler(JobExecutor):
 
-    def __init__(self, options, dir, max_threads):
+    def __init__(self, options, crawl_dir, max_threads):
         """
         Args:
             options (Options): The global configuration.
-            dir (str): The directory to crawl through.
+            crawl_dir (str): The directory to crawl through.
             max_threads (int): The maximum amount of threads to use for crawling.
         """
         super().__init__(max_threads)
@@ -62,18 +62,19 @@ class Crawler(JobExecutor):
         self._num_files = 0
         self._num_files_crawled = 0
         # URL-matching regex obtained from https://stackoverflow.com/a/3809435
-        self._url_regex = re.compile(self._options.protocol.full + r'(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)')
+        regex = r'(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
+        self._url_regex = re.compile(self._options.protocol.full + regex)
 
         # Count files and get their names and paths
-        for root, dirs, files in os.walk(dir, topdown=False):
+        for root, dummy_dirs, files in os.walk(crawl_dir, topdown=False):
             for name in files:
                 self._num_files += 1
                 path = os.path.join(root, name)
                 self._files.put((name, path))
 
-        self.start_job(False, self._run_crawls, (dir,))
+        self.start_job(False, self._run_crawls, ())
 
-    def _run_crawls(self, dir):
+    def _run_crawls(self):
         while not self.done:
             if self.job_available() and not self._files.empty():
                 index = self._num_files - self._files.qsize()
@@ -99,5 +100,5 @@ class Crawler(JobExecutor):
         self._num_files_crawled += 1
         if self._num_files_crawled == self._num_files:
             self.done = True
-        
+
         self.end_job()
--- https_enforcer.py
+++ https_enforcer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Contains the main entry point for https_enforcer"""
+
 import sys
 import signal
 
@@ -12,12 +14,25 @@ from printer import Printer
 import utils
 
 class Shutdown(Exception):
+    """This exception is used to react to shutdown signals"""
     pass
 
-def shutdown(signum, frame):
+def shutdown(_signum, _frame):
+    """Handles shutdown signals.
+
+    Raises:
+        Shutdown
+    """
     raise Shutdown
 
 def main(argv):
+    """The application entry point.
+
+    Args:
+        argv (list of str): Command line arguments.
+    """
+
+    # Print usage if wrong amount of command line arguments
     argc = len(argv)
     if argc != 2 and argc != 3:
         print('Usage: https_sentry <files directory> <optional: config file>')
@@ -27,21 +42,27 @@ def main(argv):
         print('')
         exit(1)
 
+    # Parse command line arguments
     posts_dir = argv[1]
     config_path = 'config.yaml'
     if argc == 3:
         config_path = argv[2]
 
+    # Read options from config file
     options = Options()
     options.from_yaml(utils.open_file(config_path))
 
+    # Register signal handlers
     signal.signal(signal.SIGTERM, shutdown)
     signal.signal(signal.SIGINT, shutdown)
 
+    # Launch our main objects
     crawler = Crawler(options, posts_dir, options.crawler_threads)
     net_checker = NetChecker(options, crawler, options.net_checker_threads)
     printer = Printer(options, net_checker, options.printer_threads)
 
+    # Keep the main thread alive until completion or signal
+    # All other threads run in daemon mode and will terminate along with the main thread
     main_job = JobExecutor(0)
     try:
         while not printer.done:
